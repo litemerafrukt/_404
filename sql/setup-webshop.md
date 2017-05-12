32cb16f498acd4d540e93401739f88366e1db9a2
Anders Nygren <litemerafrukt@gmail.com>
2017-05-12 15:42:55 +0200

    sql-webshop

---
+++ sql/setup-webshop.md
@@ -0,0 +1,88 @@
+# Webshop kmom05
+
+## Generellt
+Webshoppen har ett lager och ett virtuellt lager. Lagret visar de faktiska mängder av produkter som finns. Det virtuella lagret visar hur många produkter som finns tillgängliga om man räknar bort de som ligger i varukorgar. Det virtuella lagret har ingen egen tabell utan är en implementerat med vyer.
+
+Var användare (shopper) får en varukorg.
+
+Varukorgen är en enda tabell och för att se en enskild användares varukorg används en vy.
+
+Om en användare vill lägga varor i sin varukorg görs en koll mot det virtuella lagret. När en order läggs görs en koll mot det faktiska lagret med en rollback om det faktiska lagret är för litet för ordern. När det läggs en order töms användarens varukorg.
+
+När det läggs en order görs en kontroll via en trigger. Om lagersaldot för en vara går under fem läggs varan till en tabell, oophp_lowInventory, tillsammans med en tidsstämpel. För att få en rapport om varor som börjar ta slut används en vy.
+
+Alla tabeller är namespacade med oophp. Jag har använt snakecase för tabellkolumnernas namn och camelCase för argument och variabler i funktioner och procedurer.
+
+## Tabeller
+
+### oophp_prodCategories
+Produktkategorier.
+
+### oophp_products
+Produkter
+
+### oophp_inventory
+Lagret, faktiska lagervärden.
+
+### oophp_lowInventory
+Tabell över varor som gått under fem i lagersaldo.
+
+### oophp_inBaskest
+Tabell med samtliga varukorgar.
+
+### oophp_orders
+Indeviduella ordrar.
+
+### oophp_orderRows
+Tabell med samtliga befintliga orderrader.
+
+### oophp_shoppers
+Användare/varukorgar.
+
+## Vyer
+
+### oophp_vavailable
+Visar tabell över tillgänglighet i lager, hur mycket som finns i varukorgar samt hur många varor som är tillgängliga. Denna vy är framförallt en hjälpvy för oophp_vvirtualinventory.
+
+### oophp_vvirtualinventory
+Visar produkt id samt hur många som finns tillgängliga borträknat varor i varukorgar. Om det inte finns några produkter i varukorgar hämtas värdet för tillgängliga från det faktiska lagersaldot.
+
+### oophp_vbasketdetails
+Visar en översikt över varukorgstabellen.
+
+### oophp_vlowinventory
+Rapport över varor som kan lågt lagersaldo. Vyn hämtar det faktiska lagersaldot för en vara när vyn visas.
+
+### oophp_vorderdetails
+Visar översikt över alla ordrar.
+
+### oophp_vvirtualinventorydescription
+Samma sak som oophp_vvirtualinventory men inkluderar produkt description.
+
+## Procedurer
+
+### putInBasket(shopperId, prodId, amount)
+Lägg varor i en varukorg (shopperId). Påverkar inte det faktiska lagret men påverkar det virtuella lagret. En koll görs mot det virtuella lagret att det finns tillräckligt med varor kvar för att lägga i varukorgen.
+
+### getBasket(shopperId)
+Få en tabell över innehållet i en varukorg.
+
+### emptyBasket(shopperId)
+Ta bort allt innehåll i en varukorg.
+
+### placeOrder(shopperId)
+Gör om en varukorg till en order. Tar bort varorna från lagret. Allt är omslutet av en transaktion, om någon produkt får ett negativt lagervärde görs en rollback.
+
+### showOrder(orderNr)
+Visa innehållet i en order.
+
+### deleteOrder
+Tar bort en order och lägger tillbaka varorna i lagret.
+
+## Funktion
+
+### sumBasketProd(shopperId, prodId)
+Om en användare lagt till en produkt i sin varukorg flera gånger skapas det flera rader i varukorgstabellen. Denna funktion lägger ihop antalet av en produkt för en varukorg. Används för att få överskådligare utskrifter.
+
+## Övrigt
+Förutom egna procedurer och funktioner använder jag den inbyggda funktionen SUM().
---
+++ sql/setup-webshop.sql
@@ -0,0 +1,423 @@
+CREATE DATABASE IF NOT EXISTS anng15;
+USE anng15;
+
+GRANT ALL ON anng15.* TO whoever@localhost IDENTIFIED BY 'whatever';
+
+SET NAMES utf8;
+
+-- Drop tables if exists
+DROP TABLE IF EXISTS `oophp_orderRows`;
+DROP TABLE IF EXISTS `oophp_orders`;
+DROP TABLE IF EXISTS `oophp_inBaskest`;
+DROP TABLE IF EXISTS `oophp_inventory`;
+DROP TABLE IF EXISTS `oophp_lowInventory`;
+DROP TABLE IF EXISTS `oophp_shoppers`;
+DROP TABLE IF EXISTS `oophp_products`;
+DROP TABLE IF EXISTS `oophp_prodCategories`;
+
+
+DROP VIEW IF EXISTS oophp_VAvailable;
+DROP VIEW IF EXISTS oophp_VVirtualInventory;
+DROP VIEW IF EXISTS oophp_VVirtualInventoryDescription;
+DROP VIEW IF EXISTS oophp_VBasketDetails;
+DROP VIEW IF EXISTS oophp_VOrderDetails;
+DROP VIEW IF EXISTS oophp_VLowInventory;
+
+DELIMITER //
+
+-- Sum up rows of same product for a shopperId/single basket
+DROP FUNCTION IF EXISTS sumBasketProd;
+
+CREATE FUNCTION sumBasketProd(shopperId INT, prodId INT)
+RETURNS INT
+BEGIN
+    RETURN (SELECT SUM(amount) FROM InBaskets WHERE shopper_id = shopperId AND prod_id = prodId);
+END
+//
+
+DELIMITER ;
+
+--
+-- Core tables
+--
+
+CREATE TABLE `oophp_prodCategories` (
+    `id` INT AUTO_INCREMENT,
+    `description` VARCHAR(256),
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `oophp_products` (
+    `id` INT AUTO_INCREMENT,
+    `description` VARCHAR(256),
+    `image_path` VARCHAR(256),
+    `category` INT,
+    `price` INT,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`category`) REFERENCES `oophp_prodCategories` (`id`)
+);
+
+CREATE TABLE `oophp_inventory` (
+    `id` INT AUTO_INCREMENT,
+    `prod_id` INT,
+    `items` INT,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`prod_id`) REFERENCES `oophp_products` (`id`)
+);
+
+CREATE TABLE `oophp_lowInventory` (
+    `id` INT AUTO_INCREMENT,
+    `prod_id` INT,
+    `time` DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`prod_id`) REFERENCES `oophp_products` (`id`)
+);
+
+CREATE TABLE `oophp_shoppers` (
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(50),
+
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `oophp_inBaskest` (
+    `id` INT AUTO_INCREMENT,
+    `prod_id` INT,
+    `shopper_id` INT NOT NULL,
+    `amount` INT,
+
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`prod_id`) REFERENCES `oophp_products` (`id`),
+    FOREIGN KEY (`shopper_id`) REFERENCES `oophp_shoppers` (`id`)
+);
+
+CREATE TABLE `oophp_orders` (
+	`id` INT AUTO_INCREMENT,
+    `shopper_id` INT,
+    `shopper_name` VARCHAR(50),
+	`created` DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`shopper_id`) REFERENCES `oophp_shoppers` (`id`)
+);
+
+CREATE TABLE `oophp_orderRows` (
+    `id` INT AUTO_INCREMENT,
+    `order` INT,
+    `prod_id` INT,
+	`amount` INT,
+
+	PRIMARY KEY (`id`),
+	FOREIGN KEY (`order`) REFERENCES `oophp_orders` (`id`),
+	FOREIGN KEY (`prod_id`) REFERENCES `oophp_products` (`id`)
+);
+
+--
+-- Some stuff to playaround with
+--
+INSERT INTO `oophp_prodCategories` (`description`) VALUES
+("snittblommor"), ("krukväxter")
+;
+
+INSERT INTO `oophp_products` (`description`, `category`, `image_path`, `price`) VALUES
+("tulpan",   1, "image/tulip.jpg", 5),
+("ros",      1, "image/rose.jpg", 10),
+("lilja",    1, "image/lilly.jpg", 10),
+("pelargon", 2, "image/geranium.jpg", 25),
+("rosmarin", 2, "image/rosematy.jpg", 50)
+;
+
+INSERT INTO `oophp_inventory` (`prod_id`, `items`) VALUES
+(1, 100), (2, 50), (3, 30), (4, 25), (5, 10)
+;
+
+INSERT INTO `oophp_shoppers` (`name`) VALUES
+("nasse"), ("puh"), ("ior"), ("kanin")
+;
+
+INSERT INTO `oophp_inBaskest` (`prod_id`, `shopper_id`, `amount`) VALUES
+(1, 1, 30), (2, 1, 3), (1, 1, 30), (1, 2, 5)
+;
+
+
+--
+-- Virtual Inventory tables
+-- the virtual Inventory is the Inventory available excluding
+-- things in baskets.
+--
+-- Since it is a virual Inventory the actual Inventory is still
+-- unaffected.
+--
+CREATE VIEW oophp_VAvailable AS
+    SELECT
+        I.prod_id AS prod_id,
+        I.items AS inv,
+        sum(IB.amount) AS basketsums,
+        I.items - sum(IB.amount) AS available
+    FROM oophp_inBaskest AS IB
+        RIGHT OUTER JOIN oophp_inventory AS I
+            ON I.prod_id = IB.prod_id
+        GROUP BY I.prod_id;
+
+
+CREATE VIEW oophp_VVirtualInventory AS
+    SELECT
+        prod_id,
+        CASE
+            WHEN available IS NULL THEN inv
+            ELSE available
+        END AS available
+    FROM oophp_VAvailable;
+
+
+CREATE VIEW oophp_VVirtualInventoryDescription AS
+    SELECT
+        VI.prod_id AS id,
+        P.description as description,
+        VI.available as available
+    FROM oophp_VVirtualInventory AS VI
+        INNER JOIN oophp_products AS P
+            ON P.id = VI.prod_id;
+
+
+
+--
+-- Basket views
+--
+
+CREATE VIEW oophp_VBasketDetails AS
+    SELECT
+        S.id AS basket,
+        S.name AS shopper,
+        P.description AS product,
+        IB.prod_id AS prod_id,
+        sumBasketProd(S.id, IB.prod_id) AS amount
+    FROM oophp_inBaskest AS IB
+        INNER JOIN oophp_shoppers AS S
+            ON IB.shopper_id = S.id
+        INNER JOIN oophp_products AS P
+            ON IB.prod_id = P.id
+    GROUP BY amount
+    ORDER BY S.id;
+
+--
+-- Order views
+--
+
+CREATE VIEW oophp_VOrderDetails AS
+    SELECT
+        O.id AS order_nr,
+        O.shopper_id AS shopper_id,
+        O.shopper_name AS shopper_name,
+        R.id AS order_row,
+        R.prod_id AS prod_id,
+        P.description AS product,
+        R.amount AS amount
+    FROM `oophp_orders` AS O
+    	INNER JOIN oophp_orderRows AS R
+    		ON O.id = R.order
+    	INNER JOIN oophp_products AS P
+    		ON R.prod_id = P.id
+    ORDER BY order_row;
+
+--
+-- Low inventory report
+--
+
+CREATE VIEW oophp_VLowInventory AS
+    SELECT
+        I.prod_id AS 'Product id',
+        P.description AS 'Product',
+        I.items AS 'In actual inventory',
+        VI.available AS 'In virtual inventory',
+        LI.time AS 'time'
+    FROM `oophp_lowInventory`AS LI
+        INNER JOIN `oophp_products` AS P
+            ON LI.prod_id = P.id
+        INNER JOIN `oophp_inventory` AS I
+            ON LI.prod_id = I.prod_id
+        INNER JOIN `oophp_VVirtualInventory` AS VI
+            ON LI.prod_id = VI.prod_id
+    ORDER BY I.prod_id;
+
+
+--
+-- Some handy procedures
+--
+
+DELIMITER //
+
+-- Put stuff in basket
+DROP PROCEDURE IF EXISTS putInBasket;
+
+CREATE PROCEDURE putInBasket(shopperId INT, prodId INT, prodAmount INT)
+BEGIN
+    -- Check if there is enough of it
+    DECLARE prodAvailable INT;
+
+    SET prodAvailable = (SELECT available FROM oophp_VVirtualInventory WHERE prod_id = prodId);
+
+    -- Insert to oophp_inBaskest
+    IF prodAmount <= prodAvailable THEN
+        INSERT INTO `oophp_inBaskest` (`prod_id`, `shopper_id`, `amount`) VALUES (prodId, shopperId, prodAmount);
+    END IF;
+END
+//
+
+-- Show a basket
+DROP PROCEDURE IF EXISTS getBasket;
+
+CREATE PROCEDURE getBasket(shopperId INT)
+BEGIN
+    SELECT
+        prod_id,
+        SUM(amount) AS amount
+    FROM
+        oophp_inBaskest
+    WHERE shopper_id = shopperId
+    GROUP BY prod_id;
+END
+//
+
+-- Empty a basket
+DROP PROCEDURE IF EXISTS emptyBasket;
+
+CREATE PROCEDURE emptyBasket(shopperId INT)
+BEGIN
+    DELETE FROM oophp_inBaskest WHERE shopper_id=shopperId;
+    -- SELECT "Basket emptied.";
+END
+//
+
+-- Place an order.
+--
+-- Move stuff from basket to order.
+-- Rollback if not enough items in Inventory.
+--
+-- Note that checks for available items is
+-- made both here and earlier when things were put
+-- in the basket.
+DROP PROCEDURE IF EXISTS placeOrder;
+
+CREATE PROCEDURE placeOrder(shopperId INT)
+BEGIN
+
+    DECLARE leastInventoryAmount INT;
+
+    START TRANSACTION;
+
+        INSERT INTO `oophp_orders` (`shopper_id`, `shopper_name`)
+            SELECT
+                id,
+                name
+            FROM oophp_shoppers
+            WHERE id = shopperId;
+
+        INSERT INTO `oophp_orderRows` (`order`, `prod_id`, `amount`)
+        SELECT
+        	O.id AS order_nr,
+            VBD.prod_id AS prod_id,
+            VBD.amount AS amount
+        FROM `oophp_orders` AS O
+        	INNER JOIN oophp_VBasketDetails AS VBD
+        		ON O.shopper_id = VBD.basket;
+
+        UPDATE oophp_inventory AS I
+            INNER JOIN oophp_VBasketDetails AS VBD
+                ON I.prod_id = VBD.prod_id
+            SET I.items = I.items - VBD.amount
+            WHERE VBD.basket = shopperId;
+
+        -- Check that nothing in the inventory went below 0, if so rollback everything.
+        SET leastInventoryAmount = (SELECT MIN(items) FROM oophp_inventory);
+        IF leastInventoryAmount < 0 THEN
+            ROLLBACK;
+            -- SELECT "Not enough inventory to make order.";
+        ELSE
+            CALL emptyBasket(shopperId);
+            COMMIT;
+            -- SELECT "Order placed";
+        END IF;
+END
+//
+
+-- Show a single order
+DROP PROCEDURE IF EXISTS showOrder;
+
+CREATE PROCEDURE showOrder(orderNr INT)
+BEGIN
+    SELECT * FROM oophp_VOrderDetails WHERE order_nr = orderNr;
+END
+//
+
+-- Delete an order
+--
+-- Stuff get back into inventory.
+-- Stuff do not get back into basket.
+DROP PROCEDURE IF EXISTS deleteOrder;
+
+CREATE PROCEDURE deleteOrder(orderNr INT)
+BEGIN
+    UPDATE oophp_inventory AS I
+        INNER JOIN oophp_VOrderDetails AS VOD
+            ON I.prod_id = VOD.prod_id
+        SET I.items = I.items + VOD.amount
+        WHERE VOD.order_nr = orderNr;
+
+    DELETE FROM `oophp_orderRows` WHERE `order`=orderNr;
+    DELETE FROM `oophp_orders` WHERE `id`=orderNr;
+END
+//
+DELIMITER ;
+
+--
+-- Trigger
+--
+
+DELIMITER //
+
+DROP TRIGGER IF EXISTS logLowInventory;
+
+CREATE TRIGGER logLowInventory
+AFTER UPDATE
+ON oophp_inventory
+FOR EACH ROW
+BEGIN
+    IF NEW.items < 5 THEN
+        INSERT INTO oophp_lowInventory (`prod_id`) VALUES (NEW.prod_id);
+    END IF;
+END
+//
+
+DELIMITER ;
+--
+-- end handy procedures
+--
+
+--
+-- Output stuff!
+--
+-- select "";
+-- select * from `oophp_products`;
+--
+-- select "";
+-- select * from `oophp_inventory`;
+--
+-- select "";
+-- select * from oophp_shoppers;
+--
+-- select "";
+-- select * from oophp_inBaskest;
+--
+-- select "";
+-- select * from oophp_VAvailable;
+--
+-- select "";
+-- select * from oophp_VVirtualInventory;
+--
+-- select "";
+-- select * from oophp_VVirtualInventoryDescription;
